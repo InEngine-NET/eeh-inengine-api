25fd0f461063b9d0416a75d9d472e1d92dcc1a65
Ethan Hann <ethanhann@gmail.com>
2015-01-17 21:30:35 -0500

    Initial commit

---
+++ src/eeh-inengine-api.js
@@ -0,0 +1,37 @@
+(function (angular) {
+    'use strict';
+    var InEngineApi = function ($resource, apiUrl) {
+        this.$resource = $resource;
+        this.apiUrl = apiUrl;
+    };
+
+    InEngineApi.prototype.queryResource = function (name) {
+        return this.$resource(this.apiUrl + '/' + name).query().$promise;
+    };
+
+    InEngineApi.prototype.getCronTriggers = function () {
+        return this.queryResource('CronTrigger');
+    };
+
+    InEngineApi.prototype.getSimpleTriggers = function () {
+        return this.queryResource('SimpleTrigger');
+    };
+
+    InEngineApi.prototype.getJobTypes = function () {
+        return this.queryResource('Job');
+    };
+
+    angular.module('eehInEngine.api', ['ngResource'])
+    .provider('eehInEngineApi', function () {
+        var _apiUrl = 'http://localhost:9001/api';
+        this.apiUrl = function (value) {
+            if (angular.isUndefined(value)) {
+                return _apiUrl;
+            }
+            _apiUrl = value;
+        };
+        this.$get = function ($resource) {
+            return new InEngineApi($resource, _apiUrl);
+        };
+    });
+}(angular));
