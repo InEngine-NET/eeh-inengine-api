142290138ca4bb84945fa81f98eef99f96cd357b
Ethan Hann <ethanhann@gmail.com>
2015-01-19 14:00:25 -0500

    Flesh out and refactpr API
    * Separate configuration into a provider service.
    * Add ability to pause and delete triggers.
    * Allow time zones to be retrieved.

--- src/eeh-inengine-api.js
+++ src/eeh-inengine-api.js
@@ -1,32 +1,26 @@
 (function (angular) {
     'use strict';
-    var InEngineApi = function ($resource, apiUrl) {
-        this.$resource = $resource;
-        this.apiUrl = apiUrl;
+    var InEngineApiConfiguration = function (apiUrl, endpointNames) {
+        this._apiUrl = apiUrl;
+        this._endpointNames = endpointNames;
+        this._methods = {
+            'update': { method:'PUT' }
+        };
     };
 
-    InEngineApi.prototype.queryResource = function (name) {
-        return this.$resource(this.apiUrl + '/' + name).query().$promise;
+    InEngineApiConfiguration.prototype.apiUrl = function () {
+        return this._apiUrl;
     };
 
-    InEngineApi.prototype.getCronTriggers = function () {
-        return this.queryResource('CronTrigger');
+    InEngineApiConfiguration.prototype.endpointNames = function () {
+        return this._endpointNames;
     };
 
-    InEngineApi.prototype.getSimpleTriggers = function () {
-        return this.queryResource('SimpleTrigger');
+    InEngineApiConfiguration.prototype.methods = function () {
+        return this._methods;
     };
 
-    InEngineApi.prototype.getJobTypes = function () {
-        return this.queryResource('JobType');
-    };
-
-    InEngineApi.prototype.getTimeZones = function () {
-        return this.queryResource('TimeZone');
-    };
-
-    angular.module('eehInEngine.api', ['ngResource'])
-    .provider('eehInEngineApi', function () {
+    var InEngineApiConfigurationProvider = function () {
         var _apiUrl = 'http://localhost:9001/api';
         this.apiUrl = function (value) {
             if (angular.isUndefined(value)) {
@@ -34,8 +28,78 @@
             }
             _apiUrl = value;
         };
-        this.$get = function ($resource) {
-            return new InEngineApi($resource, _apiUrl);
+        var _endpointNames = {
+            cronTrigger: 'CronTrigger',
+            jobType: 'JobType',
+            simpleTrigger: 'SimpleTrigger',
+            timeZone: 'TimeZone'
         };
-    });
+        this.endpointNames = function (value) {
+            if (angular.isUndefined(value)) {
+                return _endpointNames;
+            }
+            _endpointNames = value;
+        };
+        this.$get = function () {
+            return new InEngineApiConfiguration(_apiUrl, _endpointNames);
+        };
+    };
+
+    var InEngineApi = function ($resource, eehInEngineApiConfiguration) {
+        this.$resource = $resource;
+        var config = eehInEngineApiConfiguration;
+        var endpointNames = config.endpointNames();
+        function getResource (endpointName ) {
+            var resourceName = config.apiUrl() + '/' + endpointName + '/:id';
+            return $resource(resourceName, {id:'@id'}, config.methods());
+        }
+        this.resources = {
+            cronTriggers:   getResource(endpointNames.cronTrigger),
+            simpleTriggers: getResource(endpointNames.simpleTrigger),
+            timeZones: getResource(endpointNames.timeZone),
+            jobTypes: getResource(endpointNames.jobType)
+        }
+    };
+
+    InEngineApi.prototype.getCronTriggers = function () {
+        return this.resources.cronTriggers.query().$promise;
+    };
+
+    InEngineApi.prototype.pauseTrigger = function (trigger, triggerResource) {
+        var request = angular.copy(trigger);
+        request.StateId = +(!request.StateId);
+        return triggerResource.update({id: trigger.Id}, request).$promise;
+    };
+
+    InEngineApi.prototype.pauseCronTrigger = function (trigger) {
+        return this.pauseTrigger(trigger, this.resources['cronTriggers']);
+    };
+
+    InEngineApi.prototype.pauseSimpleTrigger = function (trigger) {
+        return this.pauseTrigger(trigger, this.resources['simpleTriggers']);
+    };
+
+    InEngineApi.prototype.deleteCronTrigger = function (trigger) {
+        return this.resources.cronTriggers.remove(trigger).$promise;
+    };
+
+    InEngineApi.prototype.getSimpleTriggers = function () {
+        return this.resources.simpleTriggers.query().$promise;
+    };
+
+    InEngineApi.prototype.deleteSimpleTrigger = function (trigger) {
+        return this.resources.simpleTriggers.remove(trigger).$promise;
+    };
+
+    InEngineApi.prototype.getTimeZones = function () {
+        return this.resources.timeZones.query().$promise;
+    };
+
+    InEngineApi.prototype.getJobTypes = function () {
+        return this.resources.jobTypes.query().$promise;
+    };
+
+    angular.module('eehInEngine.api', ['ngResource'])
+        .provider('eehInEngineApiConfiguration', InEngineApiConfigurationProvider)
+        .service('eehInEngineApi', ['$resource', 'eehInEngineApiConfiguration', InEngineApi]);
 }(angular));
